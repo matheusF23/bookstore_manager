f5f8535404d4dafc1cfc47726d5f1dbea2f15d9b
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2020-10-10 17:47:31 -0300

    Refactores functionality for book creation'

--- src/main/java/com/matheusfigueiredo/bookstoremanager/entity/Author.java
+++ src/main/java/com/matheusfigueiredo/bookstoremanager/entity/Author.java
@@ -1,5 +1,10 @@
 package com.matheusfigueiredo.bookstoremanager.entity;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.*;
 
 @Entity
