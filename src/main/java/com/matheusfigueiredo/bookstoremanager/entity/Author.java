56d5f510ef4c39bda3e9e1d0fd46c706acd74670
Matheus Figueiredo <matheus_figueiredo18@hotmail.com>
2020-10-10 16:29:39 -0300

    Developed project entities

---
+++ src/main/java/com/matheusfigueiredo/bookstoremanager/entity/Author.java
@@ -0,0 +1,21 @@
+package com.matheusfigueiredo.bookstoremanager.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String name;
+
+    @Column(nullable = false)
+    private Integer age;
+}
